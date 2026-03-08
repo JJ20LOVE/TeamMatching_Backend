cdee9c5e2f82e505868463f25d8a6506eeb31c2b
Catalyst259 <10255101443@stu.ecnu.edu.cn>
2026-03-08 13:12:27 +0800

    把新建的sql文件从resources中删去

--- src/main/java/club/boyuan/official/teammatching/dto/request/community/CreateCommentRequest.java
+++ src/main/java/club/boyuan/official/teammatching/dto/request/community/CreateCommentRequest.java
@@ -1,8 +1,13 @@
 package club.boyuan.official.teammatching.dto.request.community;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * 创建评论请求DTO
  */
 public class CreateCommentRequest {
-    // 创建评论请求参数
+    @NotBlank
+    private String content;
+
+    private Integer ParentId;
 }
