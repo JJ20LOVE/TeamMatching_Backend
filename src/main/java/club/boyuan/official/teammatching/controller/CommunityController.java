b5ed1ce7219546968461b3c8e1986671e1dc83fe
Catalyst259 <10255101443@stu.ecnu.edu.cn>
2026-03-09 14:08:45 +0800

    完成评论帖子接口

--- src/main/java/club/boyuan/official/teammatching/controller/CommunityController.java
+++ src/main/java/club/boyuan/official/teammatching/controller/CommunityController.java
@@ -1,12 +1,18 @@
 package club.boyuan.official.teammatching.controller;
 
+import club.boyuan.official.teammatching.common.utils.UserContextUtil;
 import club.boyuan.official.teammatching.dto.request.community.CommunityQueryRequest;
+import club.boyuan.official.teammatching.dto.request.community.CreateCommentRequest;
 import club.boyuan.official.teammatching.dto.request.community.CreatePostRequest;
-import club.boyuan.official.teammatching.dto.response.community.CommentResponse;
+import club.boyuan.official.teammatching.dto.response.CommonResponse;
+import club.boyuan.official.teammatching.dto.response.community.CommentCreateVO;
 import club.boyuan.official.teammatching.dto.response.community.PostDetailResponse;
 import club.boyuan.official.teammatching.dto.response.community.PostListResponse;
 import club.boyuan.official.teammatching.service.CommunityService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/community")
+@Validated
 public class CommunityController {
 
     // 1. 使用 final 关键字，确保 Service 在初始化后不会被篡改
@@ -25,10 +32,12 @@ public class CommunityController {
     public CommunityController(CommunityService communityService) {
         this.communityService = communityService;
     }
+
     @PostMapping("/post")
     public PostDetailResponse<PostDetailResponse.PostCreateVO> createPost(@Valid @RequestBody CreatePostRequest request) {
         // 模拟从 JWT 中获取当前登录用户 ID（暂时写死，之后从 SecurityContext 获取）
-        Integer currentUserId = 34343333;
+//      Integer currentUserId = UserContextUtil.getCurrentUserId();
+        Integer currentUserId = 1;
         Long newPostId = communityService.createNewPost(request, currentUserId).longValue();
         PostDetailResponse.PostCreateVO vo = new PostDetailResponse.PostCreateVO(newPostId, "发布成功，待审核");
         // 调用 Service 执行发布逻辑
@@ -37,14 +46,20 @@ public class CommunityController {
 
     @GetMapping("/posts")
     public PostListResponse getPosts(@Valid CommunityQueryRequest request,
-                                      @RequestHeader(value = "Authorization", required = false) String token) {
-        Integer currentUserId = 34343333; // 可从 token 解析
+                                     @RequestHeader(value = "Authorization", required = false) String token) {
+//      Integer currentUserId = UserContextUtil.getCurrentUserId();
+        Integer currentUserId = 1;
         List<PostListResponse.CommunityPostItem> posts = communityService.queryPostList(request, currentUserId);
         return PostListResponse.success(posts);
     }
 
     @PostMapping("/post/{postId}/comment")
-    public CommentResponse CreateComment(@PathVariable Long postId) {
-
+    public CommonResponse<CommentCreateVO> CreateComment(@PathVariable @NotNull(message = "帖子ID不能为空") @Min(1) Long postId,
+                                                         @Valid @RequestBody CreateCommentRequest request) {
+        //  Integer currentUserId = UserContextUtil.getCurrentUserId();
+        Integer currentUserId = 1;
+        Long commentId = communityService.createNewComment(postId, request, currentUserId);
+        CommentCreateVO vo = new CommentCreateVO(commentId, "评论成功");
+        return CommonResponse.ok(vo);
     }
 }
--- src/main/java/club/boyuan/official/teammatching/dto/request/community/CreateCommentRequest.java
+++ src/main/java/club/boyuan/official/teammatching/dto/request/community/CreateCommentRequest.java
@@ -1,13 +1,15 @@
 package club.boyuan.official.teammatching.dto.request.community;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 /**
  * 创建评论请求DTO
  */
+@Data
 public class CreateCommentRequest {
     @NotBlank
     private String content;
 
-    private Integer ParentId;
+    private Integer parentId;
 }
