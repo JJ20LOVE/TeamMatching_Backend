2f13e400ca9e50ef1fbfa32064e62cc8510532d4
Catalyst259 <10255101443@stu.ecnu.edu.cn>
2026-03-22 14:00:51 +0800

    Admin 5个模块完成

--- src/main/java/club/boyuan/official/teammatching/dto/response/admin/StatisticsResponse.java
+++ src/main/java/club/boyuan/official/teammatching/dto/response/admin/StatisticsResponse.java
@@ -1,8 +1,22 @@
 package club.boyuan.official.teammatching.dto.response.admin;
 
-/**
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/*
  * 统计数据响应DTO
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StatisticsResponse {
     // 统计数据响应数据
+    private Integer dau;
+    private Integer newUsers;
+    private Integer projectsPublished;
+    private Integer teamsFormed;
+    private Number contentAuditPassRate;
 }
